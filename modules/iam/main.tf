4909d648cb97e9fb44fed2e36cffc322efbb454d
Mendhe <piyush.mendhe@adastragrp.com>
2025-04-10 14:34:50 +0530

    Importing IAM role lambda

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -15,4 +15,21 @@ resource "aws_iam_role" "pmendhe-Glue-role" {
   })
 }
 
+resource "aws_iam_role" "pmendhe_lambda_function" {
+  name = "pmendhe-Lambda-function"
+  path = "/service-role/"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+
 
